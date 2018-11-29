8cca930cd866a8ddb4bae4b75e1c206ff4f2fce6
JakeVidal <jrvidal@shaw.ca>
2018-11-29 11:55:12 -0800

    removed artifacts and switched to sprite graphics

--- testclient.py
+++ testclient.py
@@ -4,31 +4,11 @@ import math as m
 import random as r
 import os
 import socket
-import time
 
 # Global variables
 width, height, center = 500, 250, (150, 125)
-first_iteration = True
-xref_point = (50, 0, 0)
-yref_point = (0, -50, 0)
-zref_point = (0, 0, 50)
 HOST = '127.0.0.1'
 PORT = 10000 # this is arbitrary, must be >1024
-
-# Define rotation functions
-def rotate_point_yaxis(xcoord, ycoord, zcoord, theta):
-    return (xcoord*m.cos(theta)+zcoord*m.sin(theta), ycoord, -1*xcoord*m.sin(theta)+zcoord*m.cos(theta))
-
-def rotate_point_xaxis(xcoord, ycoord, zcoord, theta):
-    return (xcoord, ycoord*m.cos(theta)-zcoord*m.sin(theta), ycoord*m.sin(theta)+zcoord*m.cos(theta))
-
-def rotate_point_zaxis(xcoord, ycoord, zcoord, theta):
-    return (xcoord*m.cos(theta)-ycoord*m.sin(theta), xcoord*m.sin(theta)+ycoord*m.cos(theta), zcoord)
-
-def rotate_point_xyz(xcoord, ycoord, zcoord, thetax, thetay, thetaz):
-    a = rotate_point_xaxis(xcoord, ycoord, zcoord, thetax)
-    b = rotate_point_yaxis(a[0], a[1], a[2], thetay)
-    return rotate_point_zaxis(b[0], b[1], b[2], thetaz)
 
 # Add tkinter widgets placing pygame in embed
 root = tk.Tk()
@@ -36,8 +16,6 @@ root.title("Client application")
 #root.iconbitmap('icon.ico')
 embed = tk.Frame(root, width=width, height=height)
 embed.pack()
-sliderz = tk.Scale(root, from_=-180, to=180, orient=tk.HORIZONTAL, label='Z-axis')
-sliderz.pack(side=tk.RIGHT)
 slidery = tk.Scale(root, from_=-180, to=180, orient=tk.HORIZONTAL, label='Y-axis')
 slidery.pack(side=tk.RIGHT)
 sliderx = tk.Scale(root, from_=-180, to=180, orient=tk.HORIZONTAL, label='X-axis')
@@ -57,29 +35,29 @@ screen = pg.display.set_mode((width,height))
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-testnum = 0
+# Set screen background
+background = pg.Surface((width,height))
+background = background.convert()
+background.fill((100,100,100))
+screen.blit(background, (0,0))
+
+class Character(pg.sprite.Sprite):
+	def __init__(self):
+		pg.sprite.Sprite.__init__(self)
+		self.image = pg.image.load('character.png')
+		self.rect = self.image.get_rect()
+
+	def update(self, xcoord, ycoord):
+		self.rect.center = (xcoord,ycoord)
+
+# create a sprite group
+sprites = pg.sprite.Group()
+
 # Main animation loop
 while 1:
 
-    # Update  screen background
-    screen.fill((100, 100, 100))
-
-    # Set slider input
-    thetay = m.radians(sliderx.get())
-    thetax = m.radians(slidery.get())
-    thetaz = m.radians(sliderz.get())
-
-    # Draw x, y and z axis reference
-    x_point = rotate_point_xyz(xref_point[0], xref_point[1], xref_point[2], thetax, thetay, thetaz)
-    y_point = rotate_point_xyz(yref_point[0], yref_point[1], yref_point[2], thetax, thetay, thetaz)
-    z_point = rotate_point_xyz(zref_point[0], zref_point[1], zref_point[2], thetax, thetay, thetaz)
-    pg.draw.line(screen, (255, 0, 0), (75, height-75), (75+x_point[0], height-75+x_point[1]), 3)
-    pg.draw.line(screen, (0, 255, 0), (75, height-75), (75+y_point[0], height-75+y_point[1]), 3)
-    pg.draw.line(screen, (0, 0, 255), (75, height-75), (75+z_point[0], height-75+z_point[1]), 3)
-    pg.draw.circle(screen, (255, 255, 255), (75, height-75), 3)
-
-    xcoord = sliderx.get()
-    ycoord = slidery.get()
+    xcoord = int((sliderx.get() + 180)*1.39)
+    ycoord = int((slidery.get() + 180)*0.69)
        
     cmd = 'xcoord:' + str(xcoord)
     s.sendall(cmd.encode('utf8'))
@@ -91,10 +69,33 @@ while 1:
 
     print(playerdata)
     playerdata = playerdata.split(';')
+
+    numplayers = len(playerdata)-1
+    while len(sprites) != numplayers:
+    	if len(sprites) < numplayers:
+    		p = Character()
+    		sprites.add(p)
+    	elif len(sprites) > numplayers:
+    		p = Character()
+    		sprites.remove()
+
+    playerlocation = []
+
     for player in playerdata:
         player = player.split(':')
         if player != ['']:
-            pg.draw.circle(screen, (255, 255, 255), (int(player[0]), int(player[1])), 3)
+            playerlocation += [(int(player[0]), int(player[1]))]            
+
+    num = 0
+    for sprite in sprites:
+        sprite.update(playerlocation[num][0], playerlocation[num][1])
+        num += 1
+
+    #clear screen before next frame
+    screen.blit(background, (0, 0))
+
+    #draw sprites to screen
+    sprites.draw(screen)	
 
     # Update pygame display
     pg.display.flip()
